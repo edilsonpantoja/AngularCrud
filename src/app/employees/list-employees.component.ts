4813df8e6e4de94989dcc00b57932d2a48e42614
Edilson Pantoja <edilsonpantoja@hotmail.com>
2018-10-04 21:42:35 -0400

    Lesson 50

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -2,13 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 import { EmployeeService } from './employee.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './list-employees.component.html',
   styleUrls: ['./list-employees.component.css']
 })
+
 export class ListEmployeesComponent implements OnInit {
+  employees: Employee[];
+  // Use this property to stored filtered employees, so we do not
+  // lose the original list and do not have to make a round trip
+  // to the web server on every new search
+  filteredEmployees: Employee[];
+
+  private _searchTerm: string;
+
+  // We are binding to this property in the view template, so this
+  // getter is called when the binding needs to read the value
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+
+  // This setter is called everytime the value in the search text box changes
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
+
+  constructor(private _employeeService: EmployeeService,
+    private _router: Router,
+    private _route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.filteredEmployees = this.employees;
+  }
+
+  onClick(employeeId: number) {
+    this._router.navigate(['/employees', employeeId]);
+  }
+
+  filterEmployees(searchString: string) {
+    return this.employees.filter(employee =>
+      employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
+}
+
+/* export class ListEmployeesComponent implements OnInit {
   employees: Employee[];
   employeeToDisplay: Employee;
   dataFromChild: Employee;
@@ -45,4 +86,4 @@ export class ListEmployeesComponent implements OnInit {
     }
   } */
 
-}
+//}
